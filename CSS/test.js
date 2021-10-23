20dec14cb6690cdb01b6ad9faec524a859e1e200
NihalKPatel <70577135+NihalKPatel@users.noreply.github.com>
2021-10-24 09:21:24 +1300

    Initial Setup

---
+++ CSS/test.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+function addquestion(){
+var question = myform.inputquestion.value;
+var varquestion = myform.inputquestion.value;
+document.getElementById("Writequestion").innerHTML;
+}
+
+function addquestion() {
+    var person = prompt("Please enter your Email:");
+    document.getElementById("Writequestion").innerHTML="Question Sent.";
+}
